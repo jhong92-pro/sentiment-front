802a8da1359980fe9da0ffb928a5475bc90750d3
jhong92 <hjsky1010@gmail.com>
2022-12-26 00:40:44 +0900

    prod config file changed (temp)

--- rollup.prod.config.js
+++ rollup.prod.config.js
@@ -1,11 +1,26 @@
 import { terser } from "rollup-plugin-terser";
 import rollupCommonConfig from "./rollup.common.config";
+import replace from "@rollup/plugin-replace";
+import serve from 'rollup-plugin-serve';
+
 
 const config = {...rollupCommonConfig}
 
+let serverUrl = "http://localhost:7080" // will be changed
+
 config.plugins=[
     ...config.plugins,
-    terser()
+    serve({
+        host: 'localhost',
+        port:8080,
+        open:true,
+        contentBase:'dist'
+    }),
+    terser(),
+    replace({
+        preventAssignment: true,
+        "env.serverUrl": serverUrl
+      })
 ];
 
 export default config;
